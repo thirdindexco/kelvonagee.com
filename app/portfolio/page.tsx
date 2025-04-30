fcee25db0a34ca240921a94eb0c72397b07c9992
michael <mikecicc@gmail.com>
2025-04-30 19:40:34 -0400

    update copy

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,7 +5,7 @@ export default function Work() {
     <main className="pt-[52px] md:pt-[96px]">
       <div className="flex flex-col w-full">
         <div className="caption pb-1 sticky top-10">
-          Selected work (2013—2025)
+          Film and digital photography
         </div>
         <Photos />
       </div>
