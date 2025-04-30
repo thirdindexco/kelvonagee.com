92e43790a96aea5be334f2be34a724447e7ff431
michael <mikecicc@gmail.com>
2025-04-30 19:34:07 -0400

    update work to portfolio, add redirect

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    loader: "custom",
-    loaderFile: "./lib/cloudinary.ts",
+    loader: 'custom',
+    loaderFile: './lib/cloudinary.ts',
   },
-};
+  async redirects() {
+    return [
+      {
+        source: '/work',
+        destination: '/portfolio',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
